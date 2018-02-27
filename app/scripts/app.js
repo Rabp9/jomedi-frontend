8306fe1103dfe6b36a2a9f291e52e3b8026952d2
tmtrbocanegra <rabp_91@hotmail.com>
2018-02-27 16:18:44 -0500

    Primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jomediFrontendApp
+ * @description
+ * # jomediFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jomediFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
